2b96d933a246b075ec1a035eb7e8883bc1ddff2f
stilyan91 <stilyan91@gmail.com>
2023-11-15 22:50:22 +0200

    test-for-login-button-visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,9 +1,17 @@
 const { expect, test } = require("@playwright/test");
 
-test('Verify All Books links are visible', async ({ page }) => {
+test('Verify All Books link is visible', async ({ page }) => {
     await page.goto("http://localhost:3001");
     await page.waitForSelector("#site-header > nav");
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isAllBooksLinkVisible = await allBooksLink.isVisible();
     expect(isAllBooksLinkVisible).toBe(true);
+});
+
+test('Verify Login button is visible', async ({ page }) => {
+    await page.goto("http://localhost:3001");
+    await page.waitForSelector("#site-header > nav");
+    const loginButton = await page.$('#guest > a:nth-child(1)');
+    const isLoginButton = await loginButton.isVisible();
+    expect(isLoginButton).toBe(true);
 });
